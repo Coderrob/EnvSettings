32adba0adaf05a55c6fcf00a4c345284d6e54141
Coderrob <Rob@coderrob.com>
2016-05-26 10:25:41 -0500

    (initial) Initial checkin.

---
+++ EnvSettings.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Utilities
+{
+    public static class EnvSettings
+    {
+        public static string Get(string key)
+        {
+            return Environment.GetEnvironmentVariable(key);
+        }
+
+        public static string Get(string key, EnvironmentVariableTarget target)
+        {
+            return Environment.GetEnvironmentVariable(key, target);
+        }
+
+        public static TValue Get<TValue>(string key)
+        {
+            var value = Get(key);
+
+            if (string.IsNullOrEmpty(value))
+            {
+                return default(TValue);
+            }
+
+            return (TValue) Convert.ChangeType(value, typeof (TValue));
+        }
+
+        public static TValue Get<TValue>(string key, EnvironmentVariableTarget target)
+        {
+            var value = Get(key, target);
+
+            if (string.IsNullOrEmpty(value))
+            {
+                return default(TValue);
+            }
+
+            return (TValue) Convert.ChangeType(value, typeof (TValue));
+        }
+
+        public static bool TryGet<TValue>(string key, out TValue value)
+        {
+            try
+            {
+                value = Get<TValue>(key);
+                return true;
+            }
+            catch (Exception)
+            {
+                value = default(TValue);
+                return false;
+            }
+        }
+
+        public static bool TryGet<TValue>(string key, EnvironmentVariableTarget target, out TValue value)
+        {
+            try
+            {
+                value = Get<TValue>(key, target);
+                return true;
+            }
+            catch (Exception)
+            {
+                value = default(TValue);
+                return false;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# EnvSettings
-C# wrapper around Environment.GetEnvironmentVariable
+# C# EnvSettings Utility
+
+Quick and simple wrapper around the .Net Environment.GetEnvironmentVariable() function accessing environment variables.
+
+Example uses:
+
+```
+// Get a string value by default
+EnvSettings.Get("")
+
+EnvSettings.Get("", EnvironmentVariableTarget.Machine)
+
+// Get an int value from the AppSettings
+EnvSettings.Get<int>("TimeoutValue")
+
+EnvSettings.Get<int>("TimeoutValue", EnvironmentVariableTarget.User)
+
+// A safe check around getting a numeric value
+long value;
+EnvSettings.TryGet<long>("IsntNumericValue", out value)
+
+EnvSettings.TryGet<long>("IsntNumericValue", EnvironmentVariableTarget.Process, out value)
+```
